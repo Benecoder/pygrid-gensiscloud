15f99bda6dd98fbe1e6a6624ebca91345d2adf00
Benecoder <Benedikt.heyl@googlemail.com>
2020-05-21 11:18:21 +0200

    starter file to be run by cloud-init

---
+++ build_docker_image.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -Eeux
+
+# enable repository including sources, update package cache
+
+add-apt-repository -s -u restricted
+
+VERSION=430
+is_installed=false
+
+# Ubuntu has only a single version in the repository marked as "latest" of
+# this serires.
+for (( i=0 ; i<5; i++ ))
+do
+    sudo apt install -y nvidia-utils-${VERSION} libnvidia-compute-${VERSION} \
+        nvidia-kernel-common-${VERSION} \
+        nvidia-kernel-source-${VERSION} \
+        nvidia-dkms-${VERSION}
+  sleep 2
+done
+
+# remove the module if it is inserted, blacklist it
+rmmod nouveau || echo "nouveau kernel module not loaded ..."
+echo "blacklist nouveau" > /etc/modprobe.d/nouveau.conf
+# log insertion of the nvidia module
+if modprobe nvidia ; then
+   nvidia-smi
+else
+   echo "nvidia device not found"
+fi
+if [ $is_installed = true ];
+then
+  echo "NVIDIA driver has been successfully installed."
+else
+  echo "NVIDIA driver NOT been installed."
+fi
+
